db3e564c6bf053db1051a41b35fd18540d9dbace
velosypedno <klyuchka.artur@gmail.com>
2026-03-03 12:46:10 +0200

    feat: add ability to save charts with custom names

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/velosypedno/resource-allocation/chart"
@@ -13,12 +14,16 @@ import (
 
 func main() {
 	if len(os.Args) < 3 {
-		fmt.Println("Usage: go run main.go <factory_config_path> <orders_path>")
+		fmt.Println("Usage: go run main.go <factory_config_path> <orders_path> [optional_name]")
 		os.Exit(1)
 	}
 
 	factoryConfigPath := os.Args[1]
 	ordersPath := os.Args[2]
+	customName := ""
+	if len(os.Args) > 3 {
+		customName = os.Args[3] + "_"
+	}
 
 	machinesConfig, templates, err := parser.ParseFactoryConfig(factoryConfigPath)
 	if err != nil {
@@ -46,7 +51,17 @@ func main() {
 
 	solutionChart := chart.GenerateFromSolution(solution, f.Machines, metaInfo)
 
-	outputFile, err := os.Create("bar.html")
+	outputDir := "results"
+	if err := os.MkdirAll(outputDir, 0755); err != nil {
+		fmt.Printf("Error creating directory: %v\n", err)
+		os.Exit(1)
+	}
+
+	timestamp := time.Now().Format("20060102-150405")
+	fileName := fmt.Sprintf("%splan_%s.html", customName, timestamp)
+	fullPath := filepath.Join(outputDir, fileName)
+
+	outputFile, err := os.Create(fullPath)
 	if err != nil {
 		fmt.Printf("Error creating output file: %v\n", err)
 		os.Exit(1)
@@ -59,5 +74,5 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println("Successfully generated bar.html")
+	fmt.Printf("Successfully generated %s\n", fullPath)
 }
