0054b3fff66d2bc2c39b9f486b9e36c81c798e5d
velosypedno <klyuchka.artur@gmail.com>
2026-03-05 18:33:44 +0200

    feat: use cobra to better providing args to binary

--- cmd/main.go
+++ cmd/main.go
@@ -5,38 +5,50 @@ import (
 	"os"
 	"time"
 
+	"github.com/spf13/cobra"
 	"github.com/velosypedno/resource-allocation/internal/app"
 	"github.com/velosypedno/resource-allocation/internal/parser"
 )
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: go run main.go <factory_config_path> <orders_path> [optional_name]")
+	var configPath string
+	var ordersPath string
+	var customName string
+
+	var rootCmd = &cobra.Command{
+		Use:   "scheduler",
+		Short: "Resource allocation scheduler for factory production",
+		Long:  `A tool to optimize production plans using various strategies like Simulated Annealing and Genetic Algorithms.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			machinesConfig, templates, err := parser.ParseFactoryConfig(configPath)
+			if err != nil {
+				fmt.Printf("Error parsing factory config: %v\n", err)
+				os.Exit(1)
+			}
+
+			orders, err := parser.ParseOrders(ordersPath)
+			if err != nil {
+				fmt.Printf("Error parsing orders: %v\n", err)
+				os.Exit(1)
+			}
+
+			a := app.New(machinesConfig, templates)
+			startTime := time.Date(2022, 1, 1, 0, 0, 0, 0, time.Local)
+
+			err = a.Run(startTime, orders, customName)
+			if err != nil {
+				fmt.Printf("Application run error: %v\n", err)
+				os.Exit(1)
+			}
+		},
+	}
+
+	rootCmd.Flags().StringVarP(&configPath, "config", "c", "example/config.json", "path to factory configuration file")
+	rootCmd.Flags().StringVarP(&ordersPath, "orders", "o", "example/order.json", "path to orders file")
+	rootCmd.Flags().StringVarP(&customName, "name", "n", "", "custom name for the output report")
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
-
-	factoryConfigPath := os.Args[1]
-	ordersPath := os.Args[2]
-	customName := ""
-	if len(os.Args) > 3 {
-		customName = "_" + os.Args[3]
-	}
-
-	machinesConfig, templates, err := parser.ParseFactoryConfig(factoryConfigPath)
-	if err != nil {
-		fmt.Printf("Error parsing factory config: %v\n", err)
-		os.Exit(1)
-	}
-
-	orders, err := parser.ParseOrders(ordersPath)
-	if err != nil {
-		fmt.Printf("Error parsing orders: %v\n", err)
-		os.Exit(1)
-	}
-
-	a := app.New(machinesConfig, templates)
-
-	startTime := time.Date(2022, 1, 1, 0, 0, 0, 0, time.Local)
-
-	err = a.Run(startTime, orders, customName)
 }
