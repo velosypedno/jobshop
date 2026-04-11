112994e1899deb8fc2d8bbd7d389cf0f4ccd7802
velosypedno <klyuchka.artur@gmail.com>
2026-04-11 11:35:14 +0300

    refactor: delete unused job methods

--- internal/base/jobs.go
+++ internal/base/jobs.go
@@ -33,47 +33,23 @@ type JobTemplate struct {
 }
 
 type Job struct {
-	ID                  JobID
-	Name                string
-	Operations          []*Operation
-	FlattenedOperations []*Operation
+	ID         JobID
+	Name       string
+	Operations []*Operation
 }
 
 func CreateJob(id JobID, template JobTemplate, lastOperationID *OperationID) Job {
 	job := Job{
-		ID:                  id,
-		Name:                template.Name,
-		Operations:          []*Operation{},
-		FlattenedOperations: []*Operation{},
+		ID:         id,
+		Name:       template.Name,
+		Operations: []*Operation{},
 	}
 
 	for _, operation := range template.Operations {
 		job.Operations = append(job.Operations, instantiateOperation(id, operation, lastOperationID))
 	}
 
-	for _, rootOp := range job.Operations {
-		job.buildFlattenedList(rootOp)
-	}
-
 	return job
-}
-
-func (j *Job) buildFlattenedList(op *Operation) {
-	for _, child := range op.ChildOperations {
-		j.buildFlattenedList(child)
-	}
-	j.FlattenedOperations = append(j.FlattenedOperations, op)
-}
-
-func (j *Job) OperationsCount() int {
-	return len(j.FlattenedOperations)
-}
-
-func (j *Job) GetOperation(index int) *Operation {
-	if index < 0 || index >= len(j.FlattenedOperations) {
-		return nil
-	}
-	return j.FlattenedOperations[index]
 }
 
 func instantiateOperation(jobID JobID, t OperationTemplate, lastOperationID *OperationID) *Operation {
@@ -128,31 +104,4 @@ func (o *Operation) formatTree(level int) string {
 		sb.WriteString(sub.formatTree(level + 1))
 	}
 	return sb.String()
-}
-
-func (j *Job) GetNeededMachineTypes() []MachineType {
-	types := []MachineType{}
-	for _, operation := range j.Operations {
-		types = append(types, (operation.GetNeededMachineTypes())...)
-	}
-	typesMap := make(map[MachineType]struct{}, 6)
-
-	for _, t := range types {
-		typesMap[t] = struct{}{}
-	}
-
-	uniqueTypes := []MachineType{}
-	for t := range typesMap {
-		uniqueTypes = append(uniqueTypes, t)
-	}
-
-	return uniqueTypes
-}
-
-func (o *Operation) GetNeededMachineTypes() []MachineType {
-	types := []MachineType{o.MachineType}
-	for _, child := range o.ChildOperations {
-		types = append(types, (child.GetNeededMachineTypes())...)
-	}
-	return types
 }
